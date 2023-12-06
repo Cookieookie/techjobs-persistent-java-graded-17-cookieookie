8aa96104fbeb553c795cdc5da5e65944e8308b20
Cookie Van <cookievan03@gmail.com>
2023-12-06 16:07:32 -0600

    updated all files from wrong clone. now working on tests.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,20 @@
---Part 1
+--Part 1 list the columns & their data types in the table as a SQL comment
 
---Part 2
+-- id INT,
+-- employer VARCHAR(255),
+-- name VARCHAR (255),
+-- skills VARCHAR(255)
 
---Part 3
+--Part 2 write a query to list the names of the employers in STl.
 
---Part 4
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
+
+--Part 3 SQL statement to remove the job table
+
+DROP TABLE job;
+
+--Part 4 write a query to return the names of all skills that are attached to jobs in alphabetical order. If a skill does not have a job listed, it should not be included in the results of this query.
+
+SELECT * FROM skill INNER JOIN job_skills ON skill.id = job_skills.skills_id WHERE job_skills.jobs_id IS NOT NULL ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,37 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private final List<Job> jobs = new ArrayList<>();
+
+    @NotNull
+    @Size(min = 3, max = 50, message = "Location must be between 3 and 50 characters.")
+    private String location;
+
+    //add a no-args constructor
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
 }
